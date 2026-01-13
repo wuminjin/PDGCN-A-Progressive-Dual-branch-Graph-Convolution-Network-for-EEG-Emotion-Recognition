7be2ec423bd8145745460bcf8d703379db13311d
wuminjin <1062550441@qq.com>
2026-01-13 11:10:10 +0800

    Add files via upload

---
+++ DSGCN.py
@@ -0,0 +1,104 @@
+from torch_geometric.nn import GCNConv, ChebConv, SAGEConv, GraphConv, DenseSAGEConv, dense_diff_pool, DenseGCNConv
+from torch.nn import Linear, Dropout, PReLU, Conv2d, MaxPool2d, AvgPool2d, BatchNorm2d
+from torch_geometric.utils import to_dense_batch, to_dense_adj, dense_to_sparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from node_location import convert_dis_m, get_ini_dis_m
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class GCNmodel(nn.Module):
+
+    def __init__(self, xdim):
+        super(GCNmodel, self).__init__()
+
+        self.xdim = xdim
+
+        self.A_dis = torch.FloatTensor(convert_dis_m(get_ini_dis_m(), 9)).to(device)
+        self.A_dyn = nn.Parameter(torch.FloatTensor(xdim[1], xdim[1]))
+        nn.init.xavier_normal_(self.A_dyn)
+
+        self.gconv_d = DenseGCNConv(xdim[2], 32)  # (44*32, 32)
+        self.gconv_s= DenseGCNConv(xdim[2], 32)  # (44*32, 32)
+
+
+    def forward(self, x):
+
+        x = x.reshape(-1, self.xdim[1], self.xdim[2])
+        outd = F.relu(self.gconv_d(x, self.A_dyn))
+        outs = F.relu(self.gconv_s(x, self.A_dis))
+        out = torch.cat((outd, outs), dim=2)
+
+        return out
+
+
+class DSGCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        drop_rate = 0.1
+        topk = 10
+        self.channels = 62
+
+        self.conv1 = Conv2d(1, 32, (5, 5))
+        self.drop1 = Dropout(drop_rate)
+        self.pool1 = MaxPool2d((1, 4))
+        self.dsgc1 = GCNmodel(xdim=[16, 62, 65 * 32])
+
+        self.conv2 = Conv2d(32, 64, (1, 5))
+        self.drop2 = Dropout(drop_rate)
+        self.pool2 = MaxPool2d((1, 4))
+        self.dsgc2 = GCNmodel(xdim=[16, 62, 15 * 64])
+
+        self.conv3 = Conv2d(64, 128, (1, 5))
+        self.drop3 = Dropout(drop_rate)
+        self.pool3 = MaxPool2d((1, 4))
+        self.dsgc3 = GCNmodel(xdim=[16, 62, 2 * 128])
+
+        self.linend = Linear(62 * 64 * 3, 3)#62 * 64 * 3
+
+
+    def forward(self, x, edge_index, batch):
+        x, mask = to_dense_batch(x, batch)
+
+        x = x.reshape(-1, 1, 5, 265)  # (Batch*channels, 1, Freq_bands, Features)
+
+        x = F.relu(self.conv1(x))
+        x = self.drop1(x)
+        x = self.pool1(x)
+
+        x1 = self.dsgc1(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.drop2(x)
+        x = self.pool2(x)
+
+        x2 = self.dsgc2(x)
+
+        x = F.relu(self.conv3(x))
+        x = self.drop3(x)
+        x = self.pool3(x)
+
+        x3 = self.dsgc3(x)
+
+        x = torch.cat([x1, x2, x3], dim=1)
+
+        x = x.reshape(-1, 62 * 64 * 3)
+        x = self.linend(x)
+        pred = F.softmax(x, 1)
+
+        return x, pred
+
+
+if __name__ == '__main__':
+    input = torch.randn(992, 1325).to(device)
+    batch = torch.zeros(992, dtype=torch.long).to(device)
+    model = DSGCN().to(device)
+    output, pred = model(x=input, edge_index=None, batch=batch)
+    print(output.shape)
+    # print(output)
+    # print(pred)
+    # total_params = sum(p.numel() for p in model.parameters())
+    # print(f"Total parameters: {total_params}")#2091475
---
+++ main.py
@@ -0,0 +1,177 @@
+import os
+import time
+import numpy as np
+import pandas as pd
+import torch
+from torch_geometric.loader import DataLoader
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.model_selection import train_test_split
+
+from datapipe import build_dataset, get_dataset
+from DSGCN import DSGCN
+from utils import set_seed, reset_seed
+
+seed = 520
+set_seed(seed)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def create_log(Network):
+    version = 1
+    print('***' * 20)
+    while (1):  # 生成唯一日志文件
+        dfile = './result/{}_{:.0f}.csv'.format(Network.__name__, version)
+        if not os.path.exists(dfile):
+            break
+        version += 1
+    print(dfile)
+    df = pd.DataFrame()  # 创建一个空的pandas DataFrame对象
+    df.to_csv(dfile)  # 将DataFrame保存到CSV文件dfile
+
+    return dfile
+
+
+def train(model, train_loader, crit, optimizer):
+    model.train()
+    loss_all = 0
+    for data in train_loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+
+        #Multiple Classes classification Loss function
+        label = torch.argmax(data.y.view(-1,classes), axis=1)
+        label = label.to(device)#, dtype=torch.long) #, dtype=torch.int64)
+
+        output, _ = model(data.x, data.edge_index, data.batch)
+
+        loss = crit(output, label)
+        loss.backward()
+        loss_all += data.num_graphs * loss.item()
+        optimizer.step() 
+
+    return loss_all / len(train_dataset)
+
+def evaluate(model, loader, save_result=False):
+    model.eval()
+
+    predictions = []
+    labels = [] 
+
+    with torch.no_grad():
+        for data in loader:
+            label = data.y.view(-1,classes)
+            data = data.to(device)
+            _, pred = model(data.x, data.edge_index, data.batch)
+            pred = pred.detach().cpu().numpy() 
+            
+            pred = np.squeeze(pred)
+            predictions.append(pred)
+            labels.append(label)
+
+    predictions = np.vstack(predictions)
+    labels = np.vstack(labels)
+
+    #AUC score estimation 
+    AUC = roc_auc_score(labels, predictions, average='macro')
+    f1 = f1_score(np.argmax(labels, axis=1), np.argmax(predictions, axis=1), average='macro')
+    #Accuracy 
+    predictions = np.argmax(predictions, axis = -1)
+    labels = np.argmax(labels, axis = -1)
+    acc = accuracy_score(labels, predictions)
+
+    return AUC, acc, f1
+
+
+if __name__ == "__main__":
+
+    subjects = 15
+    epochs = 200
+    classes = 3  # Num. of classes
+    Network = DSGCN
+
+    #1、创建日志
+    dfile = create_log(Network)
+
+    #d、若数据集未创建，创建数据集
+    build_dataset(subjects)
+    print('Cross Validation')
+
+    #3、进行训练
+    result_data = []
+    all_last_acc = []
+    all_last_AUC = []
+    for cv_n in range(0, subjects):
+        train_dataset, test_dataset = get_dataset(subjects, cv_n)
+
+        # train_dataset, val_dataset = train_test_split(train_dataset, test_size=0.d, random_state=seed4)
+        train_dataset, val_dataset = train_test_split(train_dataset, test_size=0.2, random_state=seed)
+
+        def worker_init_fn(worker_id):
+            np.random.seed(seed + worker_id)
+
+        train_loader = DataLoader(train_dataset, batch_size=16, drop_last=False, shuffle=True,
+                                  worker_init_fn=worker_init_fn)
+        val_loader = DataLoader(val_dataset, batch_size=16, worker_init_fn=worker_init_fn)
+        test_loader = DataLoader(test_dataset, batch_size=16, worker_init_fn=worker_init_fn)
+        reset_seed(seed)
+
+        model = Network().to(device)
+
+        # lr = 0.000005
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.000005, weight_decay=0.0001)
+        crit = torch.nn.CrossEntropyLoss() #
+
+        epoch_data = []
+        val_AUC_f, val_acc_f, val_f1_f = 0, 0, 0
+
+        for epoch in range(epochs):
+            t0 = time.time()
+            loss = train(model, train_loader, crit, optimizer)
+            train_AUC, train_acc, train_f1 = evaluate(model, train_loader)
+            val_AUC, val_acc, val_f1 = evaluate(model, val_loader)
+            test_AUC, test_acc, test_f1 = evaluate(model, test_loader)
+
+            epoch_data.append([str(cv_n), epoch+1, loss, train_AUC, train_acc, val_AUC, val_acc])
+            t1 = time.time()
+            # print('V{:01d}, EP{:03d}, Loss:{:.5f}, AUC:{:.2f}, Acc:{:.2f}, VAUC:{:.2f}, Vacc:{:.2f}, Time: {:.2f}'.
+            #           format(cv_n, epoch+1, loss, train_AUC, train_acc, val_AUC, val_acc, (t1-t0)))
+            print(
+                f'V{cv_n:01d}, EP{epoch + 1:03d}, Loss:{loss:.5f}, AUC:{train_AUC:.2f}, Acc:{train_acc:.2f}, VAUC:{val_AUC:.2f}, Vacc:{val_acc:.2f}, TAUC: {test_AUC:.4f}, TAcc: {test_acc:.4f}, Time: {(t1 - t0):.2f}')
+
+            if val_acc >= val_acc_f:
+                val_AUC_f, val_acc_f, val_f1_f = val_AUC , val_acc, val_f1
+                best_model_path = os.path.join("best_model/1", f'{Network.__name__}_sub{cv_n}.pth')
+                torch.save(model.state_dict(), best_model_path)
+
+        # 最终测试
+        if best_model_path:
+            # 加载验证集上性能最佳的模型
+            # model.load_state_dict(torch.load(best_model_path))
+            model.load_state_dict(torch.load(best_model_path, map_location=device, weights_only=True))
+            test_AUC, test_acc, test_f1 = evaluate(model, test_loader)
+            print('Final Test Results:')
+            print(f'Test AUC: {test_AUC:.4f}, Test Acc: {test_acc:.4f}, Test F1: {test_f1:.4f}')
+
+        # print('Results::::::::::::')
+        # print('V{:01d}, EP{:03d}, Loss:{:.3f}, AUC:{:.7f}, Acc:{:.7f}, VAUC:{:.2f}, Vacc:{:.2f}, Time: {:.2f}'.
+        #               format(cv_n, epoch+1, loss, train_AUC, train_acc, val_AUC_f, val_acc_f, (t1-t0)))
+
+        result_data.append([str(cv_n), epoch+1, loss, test_AUC, test_acc, test_f1])
+
+        df = pd.DataFrame(data=result_data, columns=['Fold', 'Epoch', 'Loss', 'test_AUC', 'test_acc', 'test_f1'])
+
+        df.to_csv(dfile)
+
+
+    df = pd.read_csv(dfile)
+    output_file = "training_results.xlsx"
+    df.to_excel(output_file, index=False, engine="openpyxl")
+
+    lastacc = ['test_acc', df['test_acc'].mean()]
+    lastauc = ['test_AUC', df['test_AUC'].mean()]
+    print(lastacc)
+    print(lastauc)
+    print('*****************')
+
+
